e6c1a9eb2342b076b8d4eed44da3672664f2f50e
Hazim Emam <84563544+HazimEmam@users.noreply.github.com>
2024-04-08 06:35:51 +0200

    initial push

---
+++ exercises/030-pointermethod2/pointermethod2.go
@@ -0,0 +1,39 @@
+// Problem:
+
+// I AM STILL GOING
+
+package main
+
+import "fmt"
+
+func main() {
+	db := userDatabase{
+		admins: []string{"clair@example.com"},
+	}
+	// We'd like to be able to add an admin.
+	// Create a method that can add users to the database.
+	db.AddAdmin("richie@example.com")
+	fmt.Println(db.IsAdmin("richie@example.com"), db.IsAdmin("ned@hack.com"))
+}
+
+type userDatabase struct {
+	admins []string
+}
+
+// Methods are usually found below the type definition in Go.
+func (user *userDatabase) AddAdmin(s string){
+	// if user.IsAdmin(s){
+	// 	fmt.print("This admin is added before");
+	// }
+	user.admins = append(user.admins, s);
+}
+// IsAdmin returns true if the email is inside the admin database
+// and false otherwise.
+func (u userDatabase) IsAdmin(email string) bool {
+	for _, got := range u.admins {
+		if got == email {
+			return true
+		}
+	}
+	return false
+}
